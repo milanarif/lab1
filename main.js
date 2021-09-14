9f596793bc5513222fdc50d606d443a58fcba73b
Shorl <milanarif@gmail.com>
2021-09-14 04:02:43 +0200

    added fetch of kanye quotes

--- main.js
+++ main.js
@@ -42,6 +42,33 @@ const checkTheme = () => {
     }
 }
 
-fetch('https://api.kanye.rest/')
-    .then(response => response.json())
-    .then(data => console.log(data));
+
+
+
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+let getKanye = function() {
+    let modalText = document.querySelector('#kanye_quote')
+    fetch('https://api.kanye.rest/')
+        .then(response => response.json())
+        .then(data => modalText.innerHTML = "\"" + (data.quote) + "\" <br> - Kanye West");
+
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
